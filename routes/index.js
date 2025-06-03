3e14c7f1cae250316a343fb4ccdb4cc02e8d0588
Beatriz Queiroz <tbeatriz002007@gmail.com>
2025-06-03 16:44:03 -0300

    :sparkles: Organizando o Conteúdo com Includes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,12 @@
 var conn = require('./../inc/db')
 var express = require('express');
+var menus = require('./../inc/menus')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-  conn.query(`
-      SELECT * FROM tb_menus ORDER BY title
-    `, (err, results) => {
-    if (err) {
-      console.log(err)
-    }
+  menus.getMenus().then(results => {
     res.render('index', {
       title: 'Restaurante Saboroso!',
       menus: results
@@ -18,42 +14,43 @@ router.get('/', function (req, res, next) {
   })
 });
 
-router.get("/contacts", function(req,res,next){
+router.get("/contacts", function (req, res, next) {
 
-  res.render('contacts',{
-      title: 'Contato - Restaurante Saboroso!',
-      background:"images/img_bg_3.jpg",
-      h1: "Diga um oi!"
+  res.render('contacts', {
+    title: 'Contato - Restaurante Saboroso!',
+    background: "images/img_bg_3.jpg",
+    h1: "Diga um oi!"
   })
 
 })
 
-router.get("/menus", function(req,res,next){
-
-  res.render('menus',{
+router.get("/menus", function (req, res, next) {
+  menus.getMenus().then(results => {
+    res.render('menus', {
       title: 'Menus - Restaurante Saboroso!',
-      background:"images/img_bg_1.jpg",
+      background: "images/img_bg_1.jpg",
       h1: "Saboreie o Nosso Menu!"
+    })
   })
 
 })
 
-router.get("/reservations", function(req,res,next){
+router.get("/reservations", function (req, res, next) {
 
-  res.render('reservations',{
-      title: 'Reservas - Restaurante Saboroso!',
-      background:"images/img_bg_2.jpg",
-      h1: "Reserve uma Mesa!"
+  res.render('reservations', {
+    title: 'Reservas - Restaurante Saboroso!',
+    background: "images/img_bg_2.jpg",
+    h1: "Reserve uma Mesa!"
   })
 
 })
 
-router.get("/services", function(req,res,next){
+router.get("/services", function (req, res, next) {
 
-  res.render('services',{
-      title: 'Serviços - Restaurante Saboroso!',
-      background:"images/img_bg_1.jpg",
-      h1: "É um prazer poder servir!"
+  res.render('services', {
+    title: 'Serviços - Restaurante Saboroso!',
+    background: "images/img_bg_1.jpg",
+    h1: "É um prazer poder servir!"
   })
 
 })
